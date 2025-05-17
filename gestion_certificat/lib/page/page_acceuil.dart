915d2efcccf7340d04d68040d523e56c02567931
BMD0910 <bmd09102000@gmail.com>
2025-05-17 18:24:18 +0000

    1er depot: mise en place du struture de notre projet

---
+++ gestion_certificat/lib/page/page_acceuil.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import './auth/page_login.dart';  // ajuste le chemin si besoin
+import '../../constants/app_colors.dart';
+
+class PageAcceuil extends StatefulWidget {
+  const PageAcceuil({super.key});
+
+  @override
+  State<PageAcceuil> createState() => _PageAcceuilState();
+}
+
+class _PageAcceuilState extends State<PageAcceuil> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Rediriger après 10 secondes
+    Timer(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) =>  LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: AppColors.primary,
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/logo5.png',
+            height: 300,
+          ),
+          const SizedBox(height: 20),
+          
+          // const SizedBox(height: 10),
+          SizedBox(
+            width: 300,
+            child: LinearProgressIndicator(
+              backgroundColor: Colors.white24,  // léger contraste
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+          ),
+
+        ],
+      ),
+    ),
+  );
+}
+}
